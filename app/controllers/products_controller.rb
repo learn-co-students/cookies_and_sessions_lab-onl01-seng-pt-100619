3ae795407ad7118398421f3ae75204607b4939d8
anikachristopher <anika.mchristoper@gmail.com>
2020-04-08 22:41:52 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+
+    def index
+    end
+    
+    def create
+        item = params[:product]
+        session[:cart] << item
+        render :index
+    end 
+end
